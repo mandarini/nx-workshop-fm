a5e5c137124d99c26061deda2bb2a97a1c6324dd
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-05 16:11:48 +0300

    remove

--- libs/my/nice/ui/katerina/README.md
+++
@@ -1,7 +0,0 @@
-# my-nice-ui-katerina
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test my-nice-ui-katerina` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/my/nice/ui/katerina/jest.config.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'my-nice-ui-katerina',
-  preset: '../../../../../jest.preset.ts',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../../coverage/libs/my/nice/ui/katerina',
-};
