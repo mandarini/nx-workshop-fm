f4a07955afe3f015d19b9c9a31f406bcc95dca22
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-05 12:46:19 +0300

    lab 06 2

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -10,6 +10,10 @@ import Typography from '@material-ui/core/Typography';
 import { Header } from '@bg-hoard/store/ui-shared';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 
+import { Route, Routes, Link } from 'react-router-dom';
+
+import { StoreFeatureGameDetail } from '@bg-hoard/store/feature-game-detail';
+
 export const App = () => {
   return (
     <>
@@ -17,38 +21,64 @@ export const App = () => {
       <div className={styles['container']}>
         <div className={styles['games-layout']}>
           {getAllGames().map((x) => (
-            <Card key={x.id} className={styles['game-card']}>
-              <CardActionArea>
-                <CardMedia
-                  className={styles['game-card-media']}
-                  image={x.image}
-                  title={x.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {x.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {x.description}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    className={styles['game-rating']}
-                  >
-                    <strong>Rating:</strong> {formatRating(x.rating)}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <Link to={`/game/${x.id}`} key={x.id}>
+              <Card key={x.id} className={styles['game-card']}>
+                <CardActionArea>
+                  <CardMedia
+                    className={styles['game-card-media']}
+                    image={x.image}
+                    title={x.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {x.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {x.description}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                      className={styles['game-rating']}
+                    >
+                      <strong>Rating:</strong> {formatRating(x.rating)}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           ))}
         </div>
       </div>
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/feature-game-detail">StoreFeatureGameDetail</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/game/:id" element={<StoreFeatureGameDetail />} />;
+      </Routes>
+      {/* END: routes */}
     </>
   );
 };
---
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import styles from './store-feature-game-detail.module.scss';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+export const StoreFeatureGameDetail = () => {
+  const params = useParams();
+  return (
+    <div className={styles['container']}>
+      <Card>
+        <CardActionArea>
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {params['id']}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+};
+
+export default StoreFeatureGameDetail;
