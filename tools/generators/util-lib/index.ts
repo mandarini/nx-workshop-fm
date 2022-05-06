5540b3abad92772e2113f84362342724d5ad146d
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-06 10:54:36 +0300

    lab13 b

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,8 +1,15 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+export default async function (
+  tree: Tree,
+  schema: { name: string; directory: string }
+) {
+  await libraryGenerator(tree, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util,scope:${schema.directory}`,
+  });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
