e378c8db22b043f07152b97328b2cc07b1bf31f2
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-06 11:49:33 +0300

    updated generator

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -1,10 +1,45 @@
-import { Tree, formatFiles } from '@nrwl/devkit';
-import { updateJson } from '@nrwl/devkit';
+import {
+  formatFiles,
+  getProjects,
+  ProjectConfiguration,
+  Tree,
+  updateJson,
+} from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  await updateJson(tree, 'nx.json', (nxJson) => {
-    nxJson.defaultProject = 'api';
-    return nxJson;
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: any[] = Object.values(projectMap);
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags.filter((tag: string) => tag.startsWith('scope:'))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+  return [...new Set(allScopes)];
+}
+
+function replaceScopes(content: string, scopes: string[]): string {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const PATTERN = /interface Schema \{\n.*\n.*\n\}/gm;
+  return content.replace(
+    PATTERN,
+    `interface Schema {
+      name: string;
+      directory: ${joinScopes};
+    }`
+  );
+}
+
+export default async function (host: Tree) {
+  const scopes = getScopes(getProjects(host));
+  updateJson(host, 'tools/generators/util-lib/schema.json', (schemaJson) => {
+    schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: scope,
+    }));
+    return schemaJson;
   });
-  await formatFiles(tree);
+  const content = host.read('tools/generators/util-lib/index.ts', 'utf-8');
+  const newContent = replaceScopes(content, scopes);
+  host.write('tools/generators/util-lib/index.ts', newContent);
+  await formatFiles(host);
 }
